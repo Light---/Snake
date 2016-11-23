f85252c59aadc90aed803f8fec96ab7808a1d4e4
Light--- <eleary01@qub.ac.uk>
2016-11-23 17:29:34 +0000

    First Commit

---
+++ snakeGame/Input.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace snakeGame
+{
+    class Input
+    {
+
+         private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, true);
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, false);
+        }
+
+        private static Hashtable keys = new Hashtable();
+
+        public static void ChangeState(Keys key, bool stste)
+            {
+                keys[key] = state;
+             }
+
+        public static bool Pressed(Keys key)
+            {
+                if (keys[key] == null)
+                    return false;
+                 return (bool) keys[key];
+            }
+
+        private void Update(object sender, EventArgs e)
+        {
+            if (Input.Pressed(Keys.Right))
+                square_x += 4;
+            if (Input.Pressed(Keys.Left))
+                square_x -= 4;
+            if (Input.Pressed(Keys.Up))
+                square_y -= 4;
+            if (Input.Pressed(Keys.Down))
+                square_y += 4;
+            pbCanvas.Invalidate();
+        }
+
+   
+
+        private void pbCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+
+            canvas.FillRectangle(Brushes.Red, new Rectangle(square_x, square_y, 16, 16));
+        }
+
+        
+
+
+
+    }
+}
---
+++ snakeGame/SnakePart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace snakeGame
+{
+    class SnakePart
+    {
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+
+        //Constructor
+        public SnakePart()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
